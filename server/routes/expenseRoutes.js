f93b5a5b2151aa9b19d460d45a66d3545172074c
Naveen <naveenr251006@gmail.com>
2026-07-12 13:34:58 +0530

    Remove project files from main

--- server/routes/expenseRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const expenseController = require('../controllers/expenseController');
-
-router.get('/', expenseController.getExpenses);
-router.post('/', expenseController.addExpense);
-
-module.exports = router;
--- server/routes/fuelRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const fuelController = require('../controllers/fuelController');
-
-router.get('/', fuelController.getFuelLogs);
-router.post('/', fuelController.addFuelLog);
-
-module.exports = router;
